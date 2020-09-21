3e79245cb2f70780c6e08e930bcc391a2c862893
CatchyCo <santosh.shinde.mca15@gmail.com>
2020-09-21 22:35:38 +0530

    'ValidationOfTemplateDriveForm'

--- src/app/Component/template-drive-form-validation/template-drive-form-validation.component.ts
+++ src/app/Component/template-drive-form-validation/template-drive-form-validation.component.ts
@@ -10,8 +10,9 @@ export class TemplateDriveFormValidationComponent implements OnInit {
 
   constructor() { }
   skills = ['COM', 'IT', 'Electronics', 'AI', 'DS'];
+  isTopisHasError = true;
   coutries = ['INDIA', 'CHINA', 'S. KOREA', 'N. KOREA', 'NEPAL', 'BHUTAN'];
-  formData = new FormData('', '', null , '', '', []);
+  formData = new FormData('', '', null, '', 'default', []);
   ngOnInit(): void {
   }
   addSkill(skill) {
@@ -19,6 +20,13 @@ export class TemplateDriveFormValidationComponent implements OnInit {
       this.formData.skills.push(skill);
     } else {
       this.formData.skills.splice(this.formData.skills.indexOf(skill), 1);
+    }
+  }
+  checkTopic(value) {
+    if (value === 'default') {
+      this.isTopisHasError = true;
+    } else {
+      this.isTopisHasError = false;
     }
   }
 }
